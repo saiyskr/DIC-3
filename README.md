5b6b836008513db92de603fb7f1096d1823d5b2a
Vamshi Krishna Kyatham <68469412+vamshikrishnakyatham@users.noreply.github.com>
2023-12-07 21:46:23 -0500

    Working changes

--- README.md
+++ README.md
@@ -1,8 +1,15 @@
-# DIC3
+# CSE 589 Project Phase 3 by Vamshi Krishna Kyatham, Sai Kiran Reddy Yeruva, Deekshith Katker.
+
 Install dependencies needed for flask using:
 
-pip install flask flask-cors pandas scikit-learn matplotlib
+- pip install flask flask-cors scikit-learn numpy pandas
+
+Make sure you are updated with scikit-learn package. To do so, use:
+
+- pip install --upgrade scikit-learn
 
 Then run the flask app by using command:
 
-python app.py
+- python app.py
+
+The previous developed ML Model has been dumped into Pickle File which is stored in models folder in the repository.
--- app.py
+++ app.py
@@ -1,16 +1,6 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS
-import pandas as pd
-import io
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, classification_report
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import make_pipeline
-from flask import Flask, render_template, send_file
-import matplotlib.pyplot as plt
-from io import BytesIO
+from flask import Flask, render_template
 import pickle
 
 app = Flask(__name__, static_folder="/static")
@@ -20,8 +10,6 @@ with open('models\wine_quality_model.pkl', 'rb') as file:
     model, min_values, max_values = pickle.load(file)
     min_values = dict(min_values)
     max_values = dict(max_values)
-    print(min_values)
-    print(max_values)
 
 @app.route('/')
 def home():
@@ -32,7 +20,6 @@ def predict():
     features = []
     wine_type = request.form['wine_type']
     wine_type_encoded = 0 if wine_type.lower() == 'red' else 1
-    # features.append(wine_type_encoded)
     
     features = [int(wine_type_encoded),
                 float(request.form['fixed_acidity']),
@@ -47,7 +34,6 @@ def predict():
                 float(request.form['sulphates']),
                 float(request.form['alcohol'])]
     
-    # print(features)
     normalized_features =[]
     normalized_features.append(wine_type_encoded)
     for key, x in zip(min_values.keys(), features[1:]):
@@ -56,17 +42,8 @@ def predict():
             pass
         else:
             denominator = max_values[key] - min_values[key]
-            print(key)
-            print(x)
             normalized_features.append((x - min_values[key]) / denominator)
-            
-    print("Original Features:")
-    print(features)
 
-    print("\nNormalized Features:")
-    print(normalized_features)
-    
-    # model:any
     prediction = model.predict([normalized_features])[0]
     
     prediction_percent = int(prediction * 100)
@@ -76,54 +53,6 @@ def predict():
         prediction_category = "Bad"
     
     return render_template('result.html', prediction=prediction, prediction_percent=prediction_percent,prediction_category=prediction_category)
-# @app.route('/')
-# def hello():
-#     return 'Hello, Flask is running!'
-
-@app.route('/getpic', methods=['GET'])
-def getpic():
-    # Generate a simple plot
-    plt.plot([1, 2, 3, 4], [10, 20, 25, 30])
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Simple Plot')
-
-    # Save the plot to a BytesIO object
-    img_buf = BytesIO()
-    plt.savefig(img_buf, format='png')
-    img_buf.seek(0)
-    plt.close()
-
-    # Send the plot to the frontend
-    return send_file(img_buf, mimetype='image/png')
-
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    try:
-        # Check if the POST request has the file part
-        if 'file' not in request.files:
-            return jsonify({'error': 'No file part'})
-
-        file = request.files['file']
-
-        # Check if the file is empty
-        if file.filename == '':
-            return jsonify({'error': 'No selected file'})
-
-        # Assuming the file is a CSV file, read it into a Pandas DataFrame
-        # df = pd.read_csv(io.StringIO(file.read().decode('utf-8')))
-        # accuracy = MLModelling(file)
-        # Perform any necessary operations on the DataFrame
-        # For example, you could run your machine learning model on this data
-
-        # Return a response, or send the processed data back to the frontend
-        return jsonify({'success': True, "message": "File uploaded successfully"})
-
-    except Exception as e:
-        return jsonify({'error': str(e)})
-    
-
 
 if __name__ == '__main__':
     app.static_folder = 'static'

6e5daabb8da9ef767e21c52d2bfd3b1610424f7e
saiyskr <saikiranreddy.yeruva@gmail.com>
2023-12-08 15:44:40 -0500

    documenting code

--- app.py
+++ app.py
@@ -1,26 +1,42 @@
-from flask import Flask, request
+# Importing the necessary libraries
+from flask import Flask, request, render_template
 from flask_cors import CORS
-from flask import Flask, render_template
 import pickle
 
-app = Flask(__name__, static_folder="/static")
+# Creating a Flask application
+app = Flask(__name__)
 CORS(app)
 
+# Loading the ML model, minimum and maximum values needed for normalization using the pickle file
 with open('models\wine_quality_model.pkl', 'rb') as file:
     model, min_values, max_values = pickle.load(file)
     min_values = dict(min_values)
     max_values = dict(max_values)
 
+# Route for the home page
 @app.route('/')
 def home():
-    return render_template('navbars.html',min_values = min_values,max_values= max_values)
+    """
+    Render the home page with form inputs for wine quality prediction.
 
+    Returns:
+    - HTML template rendering the home page.
+    """
+    return render_template('navbars.html', min_values=min_values, max_values=max_values)
+
+# Route for predicting wine quality
 @app.route('/predict', methods=['POST'])
 def predict():
-    features = []
+    """
+    Handle the prediction request based on user input.
+
+    Returns:
+    - HTML template rendering the prediction result.
+    """
+    # Extracting features from the form data
     wine_type = request.form['wine_type']
     wine_type_encoded = 0 if wine_type.lower() == 'red' else 1
-    
+
     features = [int(wine_type_encoded),
                 float(request.form['fixed_acidity']),
                 float(request.form['volatile_acidity']),
@@ -33,27 +49,29 @@ def predict():
                 float(request.form['pH']),
                 float(request.form['sulphates']),
                 float(request.form['alcohol'])]
-    
-    normalized_features =[]
+
+    # Normalize the features using min-max normalization
+    normalized_features = []
     normalized_features.append(wine_type_encoded)
     for key, x in zip(min_values.keys(), features[1:]):
-        key = key.replace("_"," ")
-        if(key == 'pH' or key == 'total sulfur dioxide'):
+        key = key.replace("_", " ")
+        if key == 'pH' or key == 'total sulfur dioxide':
             pass
         else:
             denominator = max_values[key] - min_values[key]
             normalized_features.append((x - min_values[key]) / denominator)
 
+    # Make the prediction using the loaded machine learning model
     prediction = model.predict([normalized_features])[0]
-    
-    prediction_percent = int(prediction * 100)
-    if prediction == 1:
-        prediction_category = "Good"
-    else:
-        prediction_category = "Bad"
-    
-    return render_template('result.html', prediction=prediction, prediction_percent=prediction_percent,prediction_category=prediction_category)
 
+    # Process the prediction result for rendering
+    prediction_percent = int(prediction * 100)
+    prediction_category = "Good" if prediction == 1 else "Bad"
+
+    # Render the result template with the prediction information
+    return render_template('result.html', prediction=prediction, prediction_percent=prediction_percent,
+                           prediction_category=prediction_category)
+
+# Main function to run the Flask application
 if __name__ == '__main__':
-    app.static_folder = 'static'
     app.run(debug=True)
